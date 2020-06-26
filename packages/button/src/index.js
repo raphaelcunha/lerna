5b6cf7b9559e0fb650e0c49094f12d53a3ee1d7d
Raphael Cunha <raphael.cunha@luizalabs.com>
2020-06-26 16:26:48 -0300

    update examples

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 
-const Button = styled.button`
-    background: orange;
-    color: red;
-    border-radius: 2px;
+const Button_v1 = styled.button`
+    background: white;
+    color: blue;
+    border-radius: 50px;
     cursor: pointer;
     font-size: 1rem;
-    font-weight: 300;
+    font-weight: 600;
     padding: 9px 36px;
 `;
 
-export default Button;
+// const Button_v2 = styled.button`
+//     background: grey;
+//     color: white;
+//     border-radius: 5px;
+//     cursor: pointer;
+//     font-size: 1rem;
+//     font-weight: 600;
+//     padding: 10px 36px;
+// `;
+
+// const Button_v3 = styled.button`
+//     background: red;
+//     color: blue;
+//     border-radius: 2px;
+//     cursor: pointer;
+//     font-size: 1rem;
+//     font-weight: 300;
+//     padding: 9px 36px;
+// `;
+
+export default Button_v1;
