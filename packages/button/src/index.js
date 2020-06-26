6b6cc477593ef94d8d9fd9f9e12785f39c33c616
Raphael Cunha <raphael.cunha@luizalabs.com>
2020-06-26 17:54:32 -0300

    update pipe

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 
-const Button_v1 = styled.button`
-    background: white;
-    color: blue;
-    border-radius: 50px;
-    cursor: pointer;
-    font-size: 1rem;
-    font-weight: 600;
-    padding: 10px 36px;
-`;
-
-// const Button_v2 = styled.button`
-//     background: grey;
-//     color: white;
-//     border-radius: 5px;
+// const Button_v1 = styled.button`
+//     background: white;
+//     color: blue;
+//     border-radius: 50px;
 //     cursor: pointer;
 //     font-size: 1rem;
 //     font-weight: 600;
 //     padding: 10px 36px;
 // `;
+
+const Button_v2 = styled.button`
+    background: grey;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1rem;
+    font-weight: 600;
+    padding: 10px 36px;
+`;
 
 // const Button_v3 = styled.button`
 //     background: red;
@@ -30,4 +30,4 @@ const Button_v1 = styled.button`
 //     padding: 9px 36px;
 // `;
 
-export default Button_v1;
+export default Button_v2;
