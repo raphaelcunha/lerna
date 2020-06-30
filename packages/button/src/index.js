ee1cc68b87542448134c908dbfd6d41db3ef320f
Raphael Cunha <raphael.s.cunha@gmail.com>
2020-06-30 12:26:10 -0300

    update

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
-// const Button_v1 = styled.button`
-//     background: white;
-//     color: blue;
-//     border-radius: 50px;
-//     cursor: pointer;
-//     font-size: 1rem;
-//     font-weight: 600;
-//     padding: 10px 36px;
-// `;
+const Button_v1 = styled.button`
+    background: white;
+    color: blue;
+    border-radius: 50px;
+    cursor: pointer;
+    font-size: 1rem;
+    font-weight: 600;
+    padding: 10px 36px;
+`;
 
 // const Button_v2 = styled.button`
 //     background: grey;
@@ -20,14 +20,14 @@ import styled from 'styled-components';
 //     padding: 10px 36px;
 // `;
 
-const Button_v3 = styled.button`
-    background: red;
-    color: blue;
-    border-radius: 2px;
-    cursor: pointer;
-    font-size: 1rem;
-    font-weight: 300;
-    padding: 9px 36px;
-`;
+// const Button_v3 = styled.button`
+//     background: red;
+//     color: blue;
+//     border-radius: 2px;
+//     cursor: pointer;
+//     font-size: 1rem;
+//     font-weight: 300;
+//     padding: 9px 36px;
+// `;
 
-export default Button_v3;
+export default Button_v1;
